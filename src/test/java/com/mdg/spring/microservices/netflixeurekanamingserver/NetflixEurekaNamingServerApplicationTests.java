f94fcbc2142b5a08bb285e698ffe61178ebeb1ee
Muralidhar Goud Ediga <muralidhar.g@247.ai>
2020-04-10 18:39:49 +0530

    practice

---
+++ src/test/java/com/mdg/spring/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdg.spring.microservices.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
